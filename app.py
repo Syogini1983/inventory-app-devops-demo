2de6405351759577938cf7580ed01a44c471120e
Syogini1983 <143272542+Syogini1983@users.noreply.github.com>
2025-05-12 18:41:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ def home():
     return "Welcome to TechNova's Inventory Management App!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8080)
 
